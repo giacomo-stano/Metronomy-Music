cdaa9afbba48b85d496685da03b726315f185bdb
giacomo-stano <vmfc2hrby6@privaterelay.appleid.com>
2026-09-22 22:56:15 +0200

    feat: expose native system volume slider

--- modules/metronomy-audio-controls/src/index.tsx
+++ modules/metronomy-audio-controls/src/index.tsx
@@ -13,15 +13,40 @@ import {
 
 type NativeControlProps = ViewProps;
 
+const hasSystemVolumeModule =
+  Platform.OS === 'ios' &&
+  !!requireOptionalNativeModule('MetronomySystemVolume');
+
 const hasAirPlayModule =
   Platform.OS === 'ios' &&
   !!requireOptionalNativeModule('MetronomyAirPlay');
+
+const NativeSystemVolumeView = hasSystemVolumeModule
+  ? requireNativeViewManager<NativeControlProps>('MetronomySystemVolume')
+  : null;
 
 const NativeAirPlayView = hasAirPlayModule
   ? requireNativeViewManager<NativeControlProps>('MetronomyAirPlay')
   : null;
 
+export const nativeSystemVolumeAvailable = !!NativeSystemVolumeView;
 export const nativeAirPlayAvailable = !!NativeAirPlayView;
+
+export function SystemVolumeSlider({
+  style,
+  ...props
+}: ViewProps & { style?: StyleProp<ViewStyle> }) {
+  if (!NativeSystemVolumeView) {
+    return <View {...props} style={style} />;
+  }
+
+  return (
+    <NativeSystemVolumeView
+      {...props}
+      style={[{ width: '100%', height: 34 }, style]}
+    />
+  );
+}
 
 export function AirPlayButton({
   style,
