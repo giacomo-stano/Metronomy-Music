a1f58f31bbe141a81fdbf77ebf41415e7a4e6161
giacomo-stano <vmfc2hrby6@privaterelay.appleid.com>
2026-09-22 22:54:07 +0200

    feat: restore native AirPlay picker

---
+++ modules/metronomy-audio-controls/ios/MetronomyAirPlayModule.swift
@@ -0,0 +1,9 @@
+import ExpoModulesCore
+
+public final class MetronomyAirPlayModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("MetronomyAirPlay")
+
+    View(MetronomyAirPlayView.self)
+  }
+}
