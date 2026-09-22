e3fb30da5b29e0a68194832eac6363099ad778aa
giacomo-stano <vmfc2hrby6@privaterelay.appleid.com>
2026-09-22 22:50:28 +0200

    feat: add native remote track controls

---
+++ modules/metronomy-audio-controls/ios/MetronomyRemoteControlsModule.swift
@@ -0,0 +1,59 @@
+import ExpoModulesCore
+import MediaPlayer
+
+public final class MetronomyRemoteControlsModule: Module {
+  private var nextTarget: Any?
+  private var previousTarget: Any?
+
+  public func definition() -> ModuleDefinition {
+    Name("MetronomyRemoteControls")
+
+    Events("onNextTrack", "onPreviousTrack")
+
+    Function("setEnabled") { (enabled: Bool) in
+      if enabled {
+        self.enableRemoteCommands()
+      } else {
+        self.disableRemoteCommands()
+      }
+    }
+  }
+
+  private func enableRemoteCommands() {
+    let center = MPRemoteCommandCenter.shared()
+
+    if nextTarget == nil {
+      center.nextTrackCommand.isEnabled = true
+      nextTarget = center.nextTrackCommand.addTarget { [weak self] _ in
+        guard let self else { return .commandFailed }
+        self.sendEvent("onNextTrack")
+        return .success
+      }
+    }
+
+    if previousTarget == nil {
+      center.previousTrackCommand.isEnabled = true
+      previousTarget = center.previousTrackCommand.addTarget { [weak self] _ in
+        guard let self else { return .commandFailed }
+        self.sendEvent("onPreviousTrack")
+        return .success
+      }
+    }
+  }
+
+  private func disableRemoteCommands() {
+    let center = MPRemoteCommandCenter.shared()
+
+    if let nextTarget {
+      center.nextTrackCommand.removeTarget(nextTarget)
+      self.nextTarget = nil
+    }
+    center.nextTrackCommand.isEnabled = false
+
+    if let previousTarget {
+      center.previousTrackCommand.removeTarget(previousTarget)
+      self.previousTarget = nil
+    }
+    center.previousTrackCommand.isEnabled = false
+  }
+}
