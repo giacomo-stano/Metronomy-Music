9dd133be0f286cfb115a2bc587e9e7f64b0767e5
giacomo-stano <vmfc2hrby6@privaterelay.appleid.com>
2026-09-22 22:56:01 +0200

    feat: restore native system volume control

---
+++ modules/metronomy-audio-controls/ios/MetronomySystemVolumeModule.swift
@@ -0,0 +1,9 @@
+import ExpoModulesCore
+
+public final class MetronomySystemVolumeModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("MetronomySystemVolume")
+
+    View(MetronomySystemVolumeView.self)
+  }
+}
